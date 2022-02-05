efe9361470b2422e179a95d467e4ebe30534022b
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-02-05 17:39:23 -0600

    type errors fixed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import './style.css'
 
-const downloadButton = document.querySelector('.download-button');
+type ButtonElement = HTMLElement | null;
+type ImageElement = HTMLImageElement | null;
+
+const downloadButton: ButtonElement = document.querySelector('.download-button');
+const image: ImageElement = document.querySelector('.download-image');
 downloadButton?.addEventListener('mouseover', () => {
-  document.querySelector('.download-image').src = "./src//assets/cloud_download_black_24dp.svg";
+  if (image) {
+    image.src = "./src//assets/cloud_download_black_24dp.svg";
+  }
   downloadButton.style.backgroundColor = '#fff';
   downloadButton.style.color = '#181818';
 })
 downloadButton?.addEventListener('mouseout', () => {
-  document.querySelector('.download-image').src = "./src//assets/cloud_download_white_24dp.svg";
+  if (image) {
+    image.src = "./src//assets/cloud_download_white_24dp.svg";
+  }
   downloadButton.style.backgroundColor = '#181818';
   downloadButton.style.color = '#fff';
 })
